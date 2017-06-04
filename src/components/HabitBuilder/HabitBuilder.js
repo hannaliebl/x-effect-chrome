d0538d1687cb018357282821a56b1b4a6e1aba0c
Hanna Liebl <hanna.liebl@gmail.com>
2017-06-03 18:01:08 -0700

    Add moment, allow start date to be added

--- src/components/HabitBuilder/HabitBuilder.js
+++ src/components/HabitBuilder/HabitBuilder.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 import HabitGrid from '../HabitGrid/HabitGrid';
 import HabitTitle from '../HabitTitle/HabitTitle';
 
@@ -7,29 +8,54 @@ class HabitBuilder extends Component {
     super(props)
     this.state = {
       habitTitle: "",
-      dateBegan: ""
+      startDate: ""
     }
   }
   handleTitleChange = (event) => {
     event.preventDefault()
     this.setState({habitTitle: event.target.value})
   }
+  handleDateChange = (event) => {
+    let startDate = moment().format('MMMM D, YYYY')
+    if (event.target.value === 'tomorrow') {
+      startDate = moment().add(1, 'days').format('MMMM D, YYYY')
+    }
+    this.setState({startDate: startDate})
+  }
   render() {
     const habitCreated = this.props.isHabitCreated
     if (habitCreated) {
       return (
         <div>
-          <HabitTitle title={this.state.habitTitle} />
-          <HabitGrid />
+          <HabitTitle title={this.state.habitTitle} startDate={this.state.startDate} />
+          <HabitGrid startDate={this.state.startDate}/>
         </div>
       )
     }
     return (
       <form onSubmit={this.props.onCreate}>
+        <label htmlFor="habitTitle">
+          Title:
+        </label>
         <input type="text" 
-          name="habitTitle" 
+          name="habitTitle"
+          id="habitTitle"
           value={this.state.habitTitle} 
           onChange={this.handleTitleChange} />
+        <label htmlFor="today">
+          <input type="radio"
+            name="startDate"
+            id="today"
+            value="today"
+            onChange={this.handleDateChange} /> Today
+        </label>
+        <label htmlFor="tomorrow">
+          <input type="radio"
+            name="startDate"
+            id="tomorrow"
+            value="tomorrow"
+            onChange={this.handleDateChange} /> Tomorrow
+        </label>
         <button type="submit">Create Habit</button>
       </form>
     )
--- src/components/HabitTitle/HabitTitle.js
+++ src/components/HabitTitle/HabitTitle.js
@@ -5,7 +5,7 @@ const HabitTitle = (props) => {
   return (
     <div className="title-container">
       <h1>{props.title}</h1>
-      <h2>Started: 5/27/17</h2>
+      <h2>Started: {props.startDate}</h2>
     </div>
   )
 }
