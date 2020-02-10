87872d0032b57f0a9ba1dba40f8a3a2bcc5f8f50
Hanna Liebl <hanna.liebl@gmail.com>
2020-02-09 20:21:42 -0500

    Remove serviceworker cruft, add some styles to override weird injected stylesheet in Chrome

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import XEffectContainer from "./components/XEffectContainer";
-import registerServiceWorker from "./registerServiceWorker";
 import "./index.css";
 
 ReactDOM.render(<XEffectContainer />, document.getElementById("root"));
-registerServiceWorker();
