1b5e54f5418a8da5028b9d7c3e9d568ee1cdc2d6
Hanna Liebl <hanna.liebl@gmail.com>
2020-04-14 21:36:14 -0400

    Somehow deleted index.js, fix babel-eslint colliding with CRA version

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import XEffectContainer from "./components/XEffectContainer";
+import "./index.css";
+
+ReactDOM.render(<XEffectContainer />, document.getElementById("root"));
